341d43f96ac5c2d5aacc92dbc94feb88406d469d
DeadLord <DeadLord@DESKTOP-CGEV11K>
2022-06-29 17:41:54 +0300

    Add project files.

---
+++ TestConsoleApp/Program.cs
@@ -0,0 +1,11 @@
+﻿using TestLibrary;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int a = Homework2.Task1(2, 4);
+        double b = Homework2.Task2(4, 5);
+        Console.WriteLine(b);
+    }
+}
---
+++ TestLibrary/Class1.cs
@@ -0,0 +1,46 @@
+﻿namespace TestLibrary
+{
+    public class Homework2
+    {
+        public static int Task1(int A, int B)
+        {
+            int result = 0;
+            if (A > B)
+            {
+                result = A + B;
+            }
+            else if (A == B)
+            {
+                result = A * B;
+            }
+            else if (A > B)
+            {
+                result = A - B;
+            }
+            return result;
+
+        }
+        public static double Task2(double X, double Y)
+        {
+            int result = 0;
+            if (X > 0 && Y > 0)
+            {
+                result = 1;
+            }
+            else if (X < 0 && Y > 0)
+            {
+                result = 2;
+            }
+            else if (X < 0 && Y < 0)
+            {
+                result = 3;
+            }
+            else if (X > 0 && Y < 0)
+            {
+                result = 4;
+            }
+            return result;
+
+        }
+    }
+}
